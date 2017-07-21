dbb89b7ed8748f79b794bd67d970ddd15e3328cf
Agnieszka Sekuła <hello@agsek.pl>
2017-07-21 18:44:43 +0200

    gulp added

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+/*!
+ * gulp
+ * $ npm install gulp gulp-sass gulp-autoprefixer gulp-cssnano gulp-rename gulp-notify del --save-dev
+ */
+// Load plugins
+var gulp = require('gulp'), // gulp main module
+    sass = require('gulp-sass'), // compiles sass
+    autoprefixer = require('gulp-autoprefixer'), // adds prefixes to CSS
+    cssnano = require('gulp-cssnano'), // minifies CSS
+    rename = require('gulp-rename'), // renames temp CSS and JS files
+    notify = require('gulp-notify'), // notifies on results
+    del = require('del'), // deletes files
+    concat = require('gulp-concat'), // adds files and creates one huge file either CSS or JS
+    uglify = require('gulp-uglify'), // minifies JS
+
+    // Paths
+    pathToStyles = './src/styles/',
+    pathToScripts = './src/scripts/',
+    pathToBin = './bin/',
+    pathToBinStyles = pathToBin + 'styles',
+    pathToBinScripts = pathToBin + 'scripts';
+
+// Styles
+gulp.task('styles', function () {
+    gulp.src(pathToStyles + 'main.scss')
+        .pipe(
+            sass({
+                errLogToConsole: true,
+                outputStyle: 'compressed',
+                precision: 10
+            })
+                .on('error', notify.onError(function (error) {
+                    return 'Error spotted! ' + error
+                }))
+        )
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest(pathToBinStyles))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(cssnano())
+        .pipe(gulp.dest(pathToBinStyles))
+});
+
+// Scripts
+gulp.task('scripts', function () {
+    return gulp.src(pathToScripts + '**/*.js')
+        .pipe(concat('main.js')
+            .on('error', notify.onError(function (error) {
+                return 'Error spotted! ' + error
+            })))
+        .pipe(gulp.dest(pathToBinScripts))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify()
+            .on('error', notify.onError(function (error) {
+                return 'Error spotted! ' + error
+            })))
+        .pipe(gulp.dest(pathToBinScripts))
+});
+
+// Clean build
+gulp.task('clean', function () {
+    return del(
+        [
+            pathToBinStyles,
+            pathToBinScripts
+        ],
+        {
+            force: true
+        })
+});
+
+// Default task
+gulp.task('default', ['clean'], function () {
+    gulp.start('styles', 'scripts')
+});
+
+// Watch
+gulp.task('watch', function () {
+    // Watch .scss files
+    gulp.watch(pathToStyles + '/**/*.scss', ['styles'])
+});
---
+++ src/scripts/app.js
@@ -0,0 +1,55 @@
+new Vue({
+    el: '#app',
+    delimiters: ['${', '}'],
+    data: {
+        data: JSON.parse('[{"SA419-03X":{"photos":["http://s0.reserved.com/media/catalog/product/S/A/SA419-03X-001.jpg","http://s0.reserved.com/media/catalog/product/S/A/SA419-03X-002.jpg","http://s0.reserved.com/media/catalog/product/S/A/SA419-03X-003.jpg"],"categoryUrl":"/t-shirts","price":"39,99","newPrice":"24,99","description":"wzrost modelki: 177cm","comingSoon":false,"sizes":[{"name":"S","stock":0},{"name":"M","stock":10},{"name":"L","stock":0},{"name":"XL","stock":10}]},"SA419-00X":{"photos":["http://s0.reserved.com/media/catalog/product/S/A/SA419-00X-001.jpg","http://s0.reserved.com/media/catalog/product/S/A/SA419-00X-002.jpg","http://s0.reserved.com/media/catalog/product/S/A/SA419-00X-003.jpg"],"categoryUrl":"/blouses","price":"29,99","newPrice":"19,99","description":"wzrost modelki: 176cm","comingSoon":false,"sizes":[{"name":"S","stock":10},{"name":"M","stock":10},{"name":"L","stock":10},{"name":"XL","stock":10}]},"SA419-59X":{"photos":["http://s0.reserved.com/media/catalog/product/S/A/SA419-59X-001.jpg","http://s0.reserved.com/media/catalog/product/S/A/SA419-59X-002.jpg","http://s0.reserved.com/media/catalog/product/S/A/SA419-59X-003.jpg","http://s0.reserved.com/media/catalog/product/S/A/SA419-59X-004.jpg"],"categoryUrl":"/t-shirts","price":"29,99","newPrice":null,"description":"wzrost modelki: 175cm","comingSoon":false,"sizes":[{"name":"S","stock":0},{"name":"M","stock":0},{"name":"L","stock":0},{"name":"XL","stock":0}]}}]'),
+        model: document.getElementById('colors').dataset.model,
+        productName: document.getElementById('productName').innerText,
+        sku: document.getElementById('productSku').innerText,
+        currentPhoto: 0,
+        updated: false
+    },
+    computed: {
+        photos: function () {
+            return this.data[0][this.sku].photos
+        },
+        categoryUrl: function () {
+            return this.data[0][this.sku].categoryUrl
+        },
+        price: function () {
+            return this.data[0][this.sku].price
+        },
+        newPrice: function () {
+            return this.data[0][this.sku].newPrice
+        },
+        description: function () {
+            return this.data[0][this.sku].description
+        },
+        comingSoon: function () {
+            return this.data[0][this.sku].comingSoon
+        },
+        sizes: function () {
+            return this.data[0][this.sku].sizes
+        }
+    },
+    methods: {
+        updateProductInfo: function (event) {
+            this.updated = true;
+            this.sku = this.model + '-' + event.target.id;
+        },
+        prevPhoto: function () {
+            if (this.currentPhoto > 0) {
+                this.currentPhoto--;
+            } else {
+                this.currentPhoto = this.photos.length - 1;
+            }
+        },
+        nextPhoto: function () {
+            if (this.currentPhoto < this.photos.length - 1) {
+                this.currentPhoto++;
+            } else {
+                this.currentPhoto = 0;
+            }
+        }
+    }
+});
