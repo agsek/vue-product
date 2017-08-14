154cf0d1b6e2b9099494974e72aa312d758a9880
Agnieszka Sekuła <hello@agsek.pl>
2017-08-14 06:51:32 +0200

    WIP unit tests added

---
+++ test/store/productPage/getters.spec.js
@@ -0,0 +1,16 @@
+// import { expect } from 'chai'
+// import { getters } from '/src/scripts/store/productPage/getters'
+
+describe('product page store', function () {
+    describe('getters', function () {
+        it('should return true', function () {
+            var sth = true;
+            sth.should.equal(true)
+        })
+
+        it('should return false', function () {
+            var sth = false;
+            sth.should.equal(false)
+        })
+    })
+})
